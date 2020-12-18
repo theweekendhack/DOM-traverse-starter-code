cd70bcb4a8c1c3ade56a4747f2a8f20591985055
kadeembestteaches <56241232+kadeembestteaches@users.noreply.github.com>
2020-12-18 17:18:13 -0500

    traversing the dom starter code

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+//Traversing downwards using the querySelectorAll method
+
+//selecting all the children divs within the element with the id first-section
+
+
+//selecting the element with the id first-section
+
+
+//printing the length of the NodeList
+
+
+//firstElementChild returns the first Child element nested within a particular element
+
+
+//lastElementChild returns the last Child element nested within a particular element
+
+
+//Traversing downwards using the children property
+
+//selecting all the children element within the 1st div using the children property
+//children returns an HTMLCollection
+
+
+
+//traverse sideways 
+
+
+
+
+//traverse upwards
+
